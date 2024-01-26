e0726e8aa05b0179f8b4d7d20cc336cb8d4a219b
tbq <1336957191@qq.com>
2024-01-26 14:27:43 +0800

    亮度控制调整

--- app/src/main/java/com/ivor/arduino_esp01_light/MainActivity.java
+++ app/src/main/java/com/ivor/arduino_esp01_light/MainActivity.java
@@ -97,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
+                sendMessage();
             }
         });
     }
